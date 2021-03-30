0319d3cb73bcd06ce2e031a5584720e222e69643
ianto3 <33053514+ianto3@users.noreply.github.com>
2021-03-30 23:45:25 +0200

    Completed drum kit

--- day-1/app.js
+++ day-1/app.js
@@ -2,11 +2,21 @@
 window.document.addEventListener("keydown", function (ev) {
     const keyCode = ev.keyCode;
     const keyBtn = document.querySelector(`div[data-key="${keyCode}"]`);
-    keyBtn.classList.add("playing") // activate css effect
+    if (!keyBtn) return; // breaks rest of function, avoids erros in console.
+    keyBtn.classList.add("playing"); // activate css effect
 
     const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
     audio.currentTime = 0; // starts audio from 0, allows to play in fast succesion.
     audio.play();
-
-    keyBtn.classList.remove("playing")
 });
+
+
+const keys = document.querySelectorAll(".key");
+keys.forEach(key => {
+    key.addEventListener("transitionend", ev => {
+        console.log(ev)
+        const playing = document.querySelector(".playing");
+        if (!playing) return; // breaks rest of function, avoids erros in console.
+        playing.classList.remove("playing");
+    })
+})
